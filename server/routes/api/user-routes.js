c4297ed180e775a65c1bed90455f99dee706dcf6
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-04-20 21:42:29 -0700

    added api route shells and controller route shells

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -2,8 +2,20 @@ const router = require('express').Router();
 const {
     //fill with userMethods
     createUser,
-    getSingleUser,
-    login
+    login, 
+    getUserProfile,
+    getMatchMessages,
+    getUserMatches,
+    userMatched,
+    getMatch,
+    getMatchField, 
+    getPotentialMatch,
+    getUserPreferences,
+    setUserPreferences,
+    createMessage,
+    getFilterPreferences,
+    setFilterPreferences
+
 } = require('../../controllers/user-controller');
 
 //importing middleware
@@ -13,11 +25,46 @@ const { authMiddleware } = require('../../utils/auth');
 router.route('/')
     .post(createUser);
 
-router.route('/:id')
-    .get(getSingleUser);
-
-
 router.route('/login')
     .post(login);
+
+router.route('/:id')
+    .get(getUserProfile);
+
+router.route('/:id/matches')
+    .get(getUserMatches);
+
+router.route('/:id/match/messages')
+    .get(getMatch)
+    .get(getMatchMessages);
+
+router.route('/:id/match/messages')
+    .get(getMatch)
+    .get(getMatchMessages)
+    .post(createMessage);
+
+router.route('/field')
+    .get(getMatchField);
+
+router.route('/field/potential')
+    .get(getMatchField)
+    .get(getPotentialMatch);
+
+router.route('/field/potential/matched')
+    .get(getMatchField)
+    .get(getPotentialMatch)
+    .post(userMatched);
+
+router.route('/:id/preferences')
+    .get(getUserPreferences);
+
+router.route(':id/preferences/set')
+    .get(getUserPreferences)
+    .post(setUserPreferences);
+
+router.route('/filter')
+    .get(getFilterPreferences)
+    .post(setFilterPreferences)
+
 
 module.exports = router;
