a1ef7fd49f90574050b4d0e29fb29f00a313cff4
Austie <wustinah20@gmail.com>
2023-04-30 13:28:24 -0700

    bookmark for seeds

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,6 +1,7 @@
 const db = require('../config/connection');
-const { User, Message } = require('../models');
+const { User, Message, Chat } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const messageSeeds = require('./messageSeeds.json');
 const chatSeeds = require('./chatSeeds.json');
 
 db.once('open', async () => {
@@ -9,7 +10,10 @@ db.once('open', async () => {
         await User.create(userSeeds);
 
         await Message.deleteMany({});
-        await Message.create(chatSeeds);
+        await Message.create(messageSeeds);
+        
+        await Chat.deleteMany({});
+        await Chat.create(chatSeeds);
 
         console.log('all done seeding');
         process.exit(0);
